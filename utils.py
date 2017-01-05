88d60a2f5f16537e1651f41c864ffbe0a54effcd
Fernando <f.olguin.ch@gmail.com>
2017-01-05 15:03:28 +0000

    Create get_deck function

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from itertools import product, repeat, chain
+
 SUITS_UNICODE = {'spades':u'[%s\u2660]', 'hearts':u'[%s\u2665]',
         'diamonds':u'[%s\u2666]', 'stars':u'[%s\u2663]',
         'red_joker':u'[\u2606]', 'black_joker':u'[\u2605]'}
@@ -10,3 +12,12 @@ def validate_card(value, suit):
 
     if value=='W' and suit not in ['red_joker','black_joker']:
         raise ValueError('Card value does not match a valid suit')
+
+def get_deck(ndecks=1):
+    suits = ['spades','hearts','diamonds','stars']
+    values = range(2,11)+['A','J','Q','K']
+    deck = list(repeat(list(product(values, suits)),ndecks))
+    deck = list(chain(*deck))
+    deck += [('W','red_joker'), ('W','black_joker')]*ndecks
+
+    return deck
