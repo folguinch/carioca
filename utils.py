c6965cb67d7a3de15595adefa8b56d5e259b6175
Fernando <f.olguin.ch@gmail.com>
2017-01-06 15:31:09 +0000

    Add greeting function and round definitions

--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
 from itertools import product, repeat, chain
 
 SUITS_UNICODE = {'spades':u'[%s\u2660]', 'hearts':u'[%s\u2665]',
-        'diamonds':u'[%s\u2666]', 'stars':u'[%s\u2663]',
+        'diamonds':u'[%s\u2666]', 'clubs':u'[%s\u2663]',
         'red_joker':u'[\u2606]', 'black_joker':u'[\u2605]'}
 
 VALUES = dict(zip(range(2,11)+['A','J','Q','K','W'], range(2,11)+[15,10,10,10,30]))
+
+ROUNDS = {'2T':'2 three-of-a-kind', '1T1S':'1 three-of-a-kind & 1 straight',
+        '2S':'2 straights', '3T':'3 three-of-a-kind', 
+        '2T1S':'2 three-of-a-kind & 1 straight', 
+        '1T2S':'1 three-of-a-kind & 2 straights', 'RS':'Real straight'}
 
 def validate_card(value, suit):
     assert suit in SUITS_UNICODE.keys()
@@ -23,3 +28,8 @@ def get_deck(ndecks=1, include_joker=True):
         deck += [('W','red_joker'), ('W','black_joker')]*ndecks
 
     return deck
+
+def greeting():
+    print '#'*80
+    print '{:^80}'.format('CARIOCA')
+    print '#'*80
