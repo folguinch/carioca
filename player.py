27d2d0e943dab3b5a17ba87f8938d1e956faffdf
Fernando <f.olguin.ch@gmail.com>
2017-01-06 15:58:50 +0000

    Initialize the discard

--- player.py
+++ player.py
@@ -16,6 +16,7 @@ class Players(list):
     def __init__(self, players):
         players = get_players(players)
         self.deck = None
+        self.discard = None
 
     def get_players(self, filename):
         """
@@ -30,3 +31,4 @@ class Players(list):
 
     def get_deck(self, game):
         self.deck = Deck(game=game)
+        self.discard = Discard()
