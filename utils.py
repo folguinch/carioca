332cc3a5ba90e71297730ff07966544f972098b2
Fernando <f.olguin.ch@gmail.com>
2017-01-11 17:19:02 +0000

    Fix card string size

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from itertools import product, repeat, chain
 
 SUITS_UNICODE = {'spades':u'[%s\u2660]', 'hearts':u'[%s\u2665]',
         'diamonds':u'[%s\u2666]', 'clubs':u'[%s\u2663]',
-        'red_joker':u'[\u2606]', 'black_joker':u'[\u2605]'}
+        'red_joker':u'[ \u2606]', 'black_joker':u'[ \u2605]'}
 
 VALUES = dict(zip(range(2,11)+['A','J','Q','K','W'], range(2,11)+[15,10,10,10,30]))
 
