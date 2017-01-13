718edc55eaa262d983a4f228ff237c79a1d732a3
Fernando <f.olguin.ch@gmail.com>
2017-01-13 17:41:21 +0000

    Implement lower three-of-a-kind

--- cards.py
+++ cards.py
@@ -20,7 +20,39 @@ class Discard(list):
     pass
 
 class Hand(Cards):
-    pass
+    def lower_three(self, cards):
+        low = Down()
+        value = None
+        wildcards = 0
+
+        # Validate and lower
+        for i in cards:
+            if self[i].value=='W':
+                wildcards += 1
+                if wildcards > 1:
+                    break
+                else:
+                    low.append(self[i])
+                    self[i] = None
+                    continue
+            elif value is None:
+                value = self[i].value
+                low.append(self[i])
+                self[i] = None
+            elif self[i].value == value:
+                low.append(self[i])
+                self[i] = None
+            else:
+                break
+
+        if len(low)==3:
+            self = filter(lambda x: x is not None, self)
+            return low
+        else:
+            for i, card in zip(cards, low):
+                self[i] = card
+            return []
+
 
     #def __init__(self, cards, nmax=12):
     #    self.nmax = nmax
@@ -28,3 +60,4 @@ class Hand(Cards):
 
 class Down(Cards):
     pass
+
